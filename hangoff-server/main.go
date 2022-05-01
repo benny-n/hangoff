286cef149efe8559fcce9dac3579bc4ca96e7241
benny-n <benick12@gmail.com>
2022-05-02 01:32:29 +0300

    feat: add first server API - GET word

--- hangoff-server/main.go
+++ hangoff-server/main.go
@@ -1,11 +1,66 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+const MAX_NUMBER_OF_WORKERS = 5
+
+func scanWords(w string, s []string, c chan bool) {
+	for _, wordField := range s {
+		word := strings.Split(wordField, "\"")[1]
+		if word == w {
+			c <- true
+			return
+		}
+	}
+	c <- false
+}
+
+func wordGET(c *gin.Context) {
+	word := c.Param("word")
+	resp, err := http.Get(os.Getenv("VOCABULARY_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	data := string(body)
+	dataParts := strings.Split(data, ",")
+	chunk := len(dataParts) / MAX_NUMBER_OF_WORKERS
+	ch := make(chan bool)
+	defer close(ch)
+	for i := 0; i < MAX_NUMBER_OF_WORKERS; i++ {
+		go func(i int) {
+			scanWords(word, dataParts[i*(chunk):(i+1)*(chunk)], ch)
+		}(i)
+	}
+	var isFound bool
+	for i := 0; i < MAX_NUMBER_OF_WORKERS; i++ {
+		isFound = <-ch || isFound
+	}
+	if isFound {
+		c.Data(200, "text/plain", []byte(fmt.Sprintf("%s is a valid word!", word)))
+	} else {
+		c.Data(404, "text/plain", []byte(fmt.Sprintf("%s is not a valid word!", word)))
+	}
+}
 
 func main() {
+	godotenv.Load()
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.Data(200, "text/plain", []byte("Hello World!"))
-	})
-	r.Run("0.0.0.0:5545") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.SetTrustedProxies(nil)
+	r.GET("/word/:word", wordGET)
+	port := os.Getenv("PORT")
+	r.Run(fmt.Sprintf("0.0.0.0:%v", port))
 }
