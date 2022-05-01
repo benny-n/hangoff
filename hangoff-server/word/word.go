efefb28c4b0cf0c38b0cded392378534bf6d570b
benny-n <benick12@gmail.com>
2022-05-02 02:48:32 +0300

    refactor(server): export word API to its own dir

---
+++ hangoff-server/word/word.go
@@ -0,0 +1,56 @@
+package word
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const MAX_NUMBER_OF_WORKERS = 5
+
+func scanWords(w string, s []string, c chan bool) {
+	for _, wordField := range s {
+		word := strings.Split(wordField, "\"")[1]
+		if word == w {
+			c <- true
+			return
+		}
+	}
+	c <- false
+}
+
+func WordGET(c *gin.Context) {
+	word := c.Param("word")
+	resp, err := http.Get(os.Getenv("VOCABULARY_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	data := string(body)
+	dataParts := strings.Split(data, ",")
+	chunk := len(dataParts) / MAX_NUMBER_OF_WORKERS
+	ch := make(chan bool)
+	defer close(ch)
+	for i := 0; i < MAX_NUMBER_OF_WORKERS; i++ {
+		go func(i int) {
+			scanWords(word, dataParts[i*(chunk):(i+1)*(chunk)], ch)
+		}(i)
+	}
+	var isFound bool
+	for i := 0; i < MAX_NUMBER_OF_WORKERS; i++ {
+		isFound = <-ch || isFound
+	}
+	if isFound {
+		c.Data(200, "text/plain", []byte(fmt.Sprintf("%s is a valid word!", word)))
+	} else {
+		c.Data(404, "text/plain", []byte(fmt.Sprintf("%s is not a valid word!", word)))
+	}
+}
